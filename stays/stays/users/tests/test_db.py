3dd52272accc6dd3847f152578333c8b8a5a8072
nojoven <work.cedricjoseph@gmail.com>
2023-10-15 16:30:00 +0200

    test: query DB for ProfileHasDetectedData

--- stays/stays/users/tests/test_db.py
+++ stays/stays/users/tests/test_db.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from ..models import Profile, ProfileFollowers
+from ..models import Profile, ProfileFollowers, ProfileHasDetectedData
 
 
 # Create your tests here.
@@ -11,4 +11,9 @@ def test_can_select_all_users():
 
 def test_can_select_all_followers():
     profiles = ProfileFollowers.objects.all()
+    assert profiles is not None
+
+
+def test_can_query_ProfileHasDetectedData():
+    profiles = ProfileHasDetectedData.objects.all()
     assert profiles is not None
