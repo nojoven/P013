513e66f37bc167d3efc672bf22eae2a66c43c7de
nojoven <work.cedricjoseph@gmail.com>
2023-12-16 21:59:28 +0100

    chore: remove iommi form

--- stays/stays/core/forms.py
+++ stays/stays/core/forms.py
@@ -1,8 +1,3 @@
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from .models import Publication
-from iommi import Form, Field, Action
-
-
-
-# PublishForm = Form.create(auto__model=Publication)
