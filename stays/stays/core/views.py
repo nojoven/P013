04300c0be1a77ede1ae76df490147853467e1bbd
nojoven <work.cedricjoseph@gmail.com>
2023-12-26 21:14:47 +0100

    feat: add publications to context

--- stays/stays/core/views.py
+++ stays/stays/core/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from users.models import Profile
- 
+from core.models import Publication
 
 # Create your views here.
 def home(request):
-    profiles = Profile.objects.all().filter(is_superuser=False)
+    profiles = Profile.objects.all()
+    # profiles = Profile.objects.all().filter(is_superuser=False)
+    publications = Publication.objects.all().order_by('-created_at')
     context = {
         'profiles_list': profiles,
+        'publications_list': publications
     }
-    return render(request, 'home.html', context)
+    return render(request, 'feed.html', context)
