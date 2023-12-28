397271d044a2c191fd9856a81bb1cbe30420409e
nojoven <work.cedricjoseph@gmail.com>
2023-12-28 12:07:55 +0100

    rememeber django_current_ser by writing somewhere

--- stays/stays/stays/api.py
+++ stays/stays/stays/api.py
@@ -1,6 +1,12 @@
 from ninja import NinjaAPI
 from throttle.decorators import throttle
 
+
+from django_currentuser.middleware import (
+    get_current_user, 
+    get_current_authenticated_user)
+
+
 api = NinjaAPI()
 
 @throttle(zone='default')
