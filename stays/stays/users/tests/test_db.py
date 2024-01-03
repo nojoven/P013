e8d35dfa9dfd6770b1555c1f55c3366cd24073f3
nojoven <work.cedricjoseph@gmail.com>
2024-01-03 23:58:03 +0100

    fix : remove obsolete lines

--- stays/stays/users/tests/test_db.py
+++ stays/stays/users/tests/test_db.py
@@ -1,19 +1,9 @@
 from django.test import TestCase
-from ..models import Profile, ProfileFollowers, ProfileHasDetectedData
+from ..models import Profile
 
 
 # Create your tests here.
 
 def test_can_select_all_users():
     profiles = Profile.objects.all().filter(is_superuser=False)
-    assert profiles is not None
-
-
-def test_can_select_all_followers():
-    profiles = ProfileFollowers.objects.all()
-    assert profiles is not None
-
-
-def test_can_query_ProfileHasDetectedData():
-    profiles = ProfileHasDetectedData.objects.all()
     assert profiles is not None
