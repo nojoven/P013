6447ed4b0f4e7f7f7d92ddffd98cdf4be191bc0d
nojoven <work.cedricjoseph@gmail.com>
2024-01-03 23:57:06 +0100

    chore

--- stays/stays/locations/tests/test_db.py
+++ stays/stays/locations/tests/test_db.py
@@ -1,22 +1,11 @@
 from django.test import TestCase
-from ..models import LocationCategory, Location, LocationWebData, LocationHasProfileActivity
+from ..models import StayCountry
 
 # Create your tests here.
-def test_can_select_all_location_categories():
-    categories  = LocationCategory.objects.all()
-    assert categories is not None
 
 def test_can_select_all_locations():
-    locations  = Location.objects.all()
-    assert locations is not None
+    stay_country_table  = StayCountry.objects.all()
+    assert stay_country_table is not None
 
 
-def test_can_select_all_location_web_data():
-    web_data  = LocationWebData.objects.all()
-    assert web_data is not None
-
-
-def test_can_select_all_location_profiles_activity():
-    activities  = LocationHasProfileActivity.objects.all()
-    assert activities is not None
 
