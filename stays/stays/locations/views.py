4ee29d482c6944c300023a0e8e3f71c2b6a4882b
nojoven <work.cedricjoseph@gmail.com>
2024-01-14 18:25:47 +0100

    get country data in context

--- stays/stays/locations/views.py
+++ stays/stays/locations/views.py
@@ -14,8 +14,8 @@ def country_view(request, country_code):
     # Get country data from Restcountries API
     response = requests.get(f'https://restcountries.com/v3.1/alpha/{country_code}')
     country_details = response.json()[0]
-    google_maps_url = country_details.get("maps")["googleMaps"] if country_details.get("maps") else None
-    currency = country_details.get("currencies")
+    
+    # ninjas api
     headers = {'X-Api-Key': napk}
     response_ninjas = requests.get(f'https://api.api-ninjas.com/v1/country?name={country_code}', headers=headers)
     country_details_ninjas = response_ninjas.json()
@@ -30,17 +30,19 @@ def country_view(request, country_code):
         'country_lon': country_details.get("latlng")[1],
         'country_name': country_details.get("name"),
         'country_capital': country_details.get("capital"),
-        'country_currency': currency,
+        'country_currency': country_details.get("currencies"),
         'country_population': country_details_ninjas[0].get("population"),
         'country_area': country_details_ninjas[0].get("surface_area"),
         'country_languages': country_details.get("languages", ""),
-        'google_maps_url': google_maps_url,
+        'google_maps_url': country_details.get("maps", "").get("googleMaps"),
         'gdp': country_details_ninjas[0].get("gdp"),
         'unemployment': country_details_ninjas[0].get("unemployment"),
         'gdp_growth': country_details_ninjas[0].get("gdp_growth"),
         'infant_mortality': country_details_ninjas[0].get("infant_mortality"),
         'fertility': country_details_ninjas[0].get("fertility"),
         'urban_pop_rate': country_details_ninjas[0].get("urban_population_growth"),
+        'coat_of_arms': country_details.get("coatOfArms", "").get("png", ""),
+        'flag': country_details.get("flags", f"https://flagcdn.com/w320/{country_code}.png").get("png"),
     }
 
     return render(request, 'country.html', context)
