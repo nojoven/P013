6f3223045de1b06539ba174b83777dc4bca16b93
nojoven <work.cedricjoseph@gmail.com>
2024-01-28 01:33:06 +0100

    fix: make it faster

--- stays/stays/locations/views.py
+++ stays/stays/locations/views.py
@@ -12,23 +12,67 @@ from asgiref.sync import async_to_sync
 # Create your views here.
 
 
-def country_view(request, country_code):
+async def fetch_country_data(country_code, headers):
+    async with httpx.AsyncClient() as client:
+        url1 = f'https://restcountries.com/v3.1/alpha/{country_code}'
+        url2 = f'https://api.api-ninjas.com/v1/country?name={country_code}'
+
+        responses = await asyncio.gather(
+            client.get(url1),
+            client.get(url2, headers=headers),
+        )
+
+    return responses
+
+async def fetch_additional_data(capital, country_code, headers):
+    async with httpx.AsyncClient() as client:
+        url3 = f'https://api.api-ninjas.com/v1/airquality?city={capital}'
+        url4 = f'https://api.api-ninjas.com/v1/weather?city={capital}&country={country_code}'
+        url5 = f"https://api.api-ninjas.com/v1/worldtime?city={capital}&country={country_code}"
+
+        responses = await asyncio.gather(
+            client.get(url3, headers=headers),
+            client.get(url4, headers=headers),
+            client.get(url5, headers=headers)
+        )
+
+    return responses
+
+async def country_view(request, country_code):
     # Get country data from Restcountries API
     if not isinstance(country_code, str) or len(country_code) < 2:
         return HttpResponse("Invalid link url. Please inform our support team.", status=400)
     if len(country_code) > 2:
         country_code = country_code[:2].upper()
-    response = requests.get(f'https://restcountries.com/v3.1/alpha/{country_code}')
-    country_details = response.json()[0]
 
+    context = {}
+    ninjas_api_headers = {'X-Api-Key': napk}
+
+    # Call the async function and wait for it to finish
+    responses = await fetch_country_data(country_code, ninjas_api_headers)
+
+    # Unpack the responses
+    country_details, country_details_ninjas = responses
+
+    # Process The response from Restcountries API
+    ic(country_details)
+    country_details = country_details.json()[0]
+    # Format the currency details
+    ic(country_details.get('currencies'))
+    currency_code = list(country_details.get('currencies').keys())[0]
+    currency_name = country_details.get('currencies').get(currency_code).get('name')
+    currency_symbol = country_details.get('currencies').get(currency_code).get('symbol')
+
+    # Extract the native name code (depends on the country in your httpp request)
     native_name_code = list(country_details.get("name").get("nativeName").keys())[0]
-    country_data = {
+
+    context["general_information"] = {
         'Latitude': country_details.get("latlng")[0] if country_details.get("latlng") else "N/A",
         'Longitude': country_details.get("latlng")[1] if country_details.get("latlng") else "N/A",
         'Official': country_details.get("name").get("official"),
         'Native': country_details.get("name").get("nativeName").get(native_name_code).get("official"),
         'Capital': country_details.get("capital")[0] if country_details.get("capital") else "N/A",
-        # 'Currency': f"{country_details.get('currencies').get('name')} ({country_details.get('currencies').get('symbol')})",
+        "Currency": f"{currency_name} ({currency_code}) - {currency_symbol}",
         'Languages': list(country_details.get("languages", "").values())[0],
         'Google': country_details.get("maps", "").get("googleMaps"),
         'OpenStreet': country_details.get("maps", "").get("openStreetMaps"),
@@ -36,115 +80,104 @@ def country_view(request, country_code):
         'flag': country_details.get("flags", f"https://flagcdn.com/w320/{country_code}.png").get("png"),
     }
 
-    # ninjas api
-    headers = {'X-Api-Key': napk}
-    response_ninjas = requests.get(f'https://api.api-ninjas.com/v1/country?name={country_code}', headers=headers)
-    country_details_ninjas = response_ninjas.json()
+    
+    # Start processing Ninjas API responses
+    country_data_ninjas = country_details_ninjas.json()[0]
+    
+    for key in country_data_ninjas:
+        if key == "currency":
+            continue
 
-    for key in country_details_ninjas[0]:
         # If the key is not already in the context, add it
-        if key not in country_data and key != "iso2" and "gdp not in key" and key != "Currency":
-            country_data[key.capitalize()] = country_details_ninjas[0].get(key)
+        if key not in context["general_information"] and key != "iso2" and "gdp not in key":
+            context["general_information"][key.capitalize()] = country_data_ninjas.get(key)
         
         if key == "iso2":
-            country_data["ISO_2_Code"] = country_details_ninjas[0].get(key)
+            context["general_information"]["ISO_2_Code"] = country_data_ninjas.get(key)
             
         if "gdp" in key and len(key.split("_")) > 1:
             gdp_new_key = key.capitalize().replace("gdp", "GDP")
-            country_data[gdp_new_key] = country_details_ninjas[0].get(key)
+            context["general_information"][gdp_new_key] = country_data_ninjas.get(key)
 
         if "Gdp" in key and len(key.split("_")) > 1:
             if key.startswith("Gdp") or key.startswith("gdp"):
                 updated_key = key.replace("gdp", "GDP")
-                country_data[updated_key] = country_details_ninjas[0].get(key)
+                context["general_information"][updated_key] = country_data_ninjas.get(key)
             else:
                 updated_key = key.capitalize().replace("gdp", "GDP")
-                country_data[updated_key] = country_details_ninjas[0].get(key)
+                context["general_information"][updated_key] = country_data_ninjas.get(key)
 
         if key == "GDP":
-            country_data["GDP"] = country_details_ninjas[0].get(key)
+            context["general_information"]["GDP"] = country_data_ninjas.get(key)
 
-    # ninjas overwrites restcountries "Currency"
-    # Format the currency details
-    ic(country_details.get('currencies'))
-    currency_code = list(country_details.get('currencies').keys())[0]
-    currency_name = country_details.get('currencies').get(currency_code).get('name')
-    currency_symbol = country_details.get('currencies').get(currency_code).get('symbol')
-    country_data["Currency"] = f"{currency_name} ({currency_code}) - {currency_symbol}"
+    # Call the second async function and wait for it to finish
+    if context["general_information"].get("Capital"):
+        additional_responses = await fetch_additional_data(context["general_information"]["Capital"], country_code, ninjas_api_headers)
+
+        # Unpack the responses
+        air_quality_ninjas_api_response, weather_ninjas_api_response, world_time_ninjas_api_response = additional_responses
     
-    
-    context = {"general_information": country_data}
-    
-    # Air Quality
-    if country_data.get("Capital"):
-        air_quality_ninjas_api_response = requests.get(f'https://api.api-ninjas.com/v1/airquality?city={country_data["Capital"]}', headers=headers)
+        # Extract the wanted collections from the responses
         air_quality_json = air_quality_ninjas_api_response.json()
-        if air_quality_json and "overall_aqi" in air_quality_json:
-            air_quality_json["Overall"] = air_quality_json.get("overall_aqi")
-            del air_quality_json["overall_aqi"]
+        weather_json = weather_ninjas_api_response.json()
+        world_time_json = world_time_ninjas_api_response.json()
+
+    # Air Quality
+    if air_quality_json and "overall_aqi" in air_quality_json:
+        air_quality_json["Overall"] = air_quality_json.get("overall_aqi")
+        del air_quality_json["overall_aqi"]
         context["air"] = air_quality_json
 
-    # Timezone
-    # timezone_ninjas_api_response = requests.get(f'https://api.api-ninjas.com/v1/timezone?country={country_code}', headers=headers)
-    # timezone_json = timezone_ninjas_api_response.json()[0]
-    # if timezone_json:
-    #     context["Timezone"] = timezone_json.get("timezone")
-    
-    # Weather                                         
-    weather_ninjas_api_response = requests.get(f'https://api.api-ninjas.com/v1/weather?city={country_data["Capital"]}&country={country_code}', headers=headers)
-    weather_json = weather_ninjas_api_response.json()
-    ic(weather_json)
-    # Mapping dictionary
-    key_mapping = {
-        'cloud_pct': 'Clouds',
-        'feels_like': 'Perceived',
-        'humidity': 'Humidity',
-        'max_temp': 'Max',
-        'min_temp': 'Min',
-        'sunrise': 'Sunrise',
-        'sunset': 'Sunset',
-        'temp': 'Temperature',
-        'wind_degrees': 'Wind - Degrees',
-        'wind_speed': 'Wind - Speed'
-    }
-    formatted_weather_json = {}
+    # Weather
+    if weather_json:
+        ic(weather_json)
+        # Mapping dictionary
+        key_mapping = {
+            'cloud_pct': 'Clouds',
+            'feels_like': 'Perceived',
+            'humidity': 'Humidity',
+            'max_temp': 'Max',
+            'min_temp': 'Min',
+            'sunrise': 'Sunrise',
+            'sunset': 'Sunset',
+            'temp': 'Temperature',
+            'wind_degrees': 'Wind - Degrees',
+            'wind_speed': 'Wind - Speed'
+        }
+        formatted_weather_json = {}
 
-    for wkey, value in weather_json.items():
-        ic(wkey)
-        if wkey in key_mapping:
-            new_key = key_mapping.get(wkey).capitalize()  # Get the new key from the mapping, or use the old key if not found
-            if new_key in ['Clouds', 'Humidity']:
-                new_value = f'{value} %'
-            elif new_key in ['Perceived', 'Max', 'Min', 'Temperature']:
-                new_value = f'{value} °C'
-            elif new_key == 'Wind - degrees':
-                new_key = 'Wind - Degrees'
-                new_value = f'{value}°'
-            elif new_key == 'Wind - speed':
-                new_key = 'Wind - Speed'
-                new_value = f'{value} km/h'
-            elif new_key == 'Sunrise' or new_key == 'Sunset':
-                # Convert the timestamp to a datetime object
-                dt_object = datetime.fromtimestamp(value)
+        # Improve format of the weather values
+        for wkey, value in weather_json.items():
+            if wkey in key_mapping:
+                new_key = key_mapping.get(wkey).capitalize()  # Get the new key from the mapping, or use the old key if not found
+                if new_key in ['Clouds', 'Humidity']:
+                    new_value = f'{value} %'
+                elif new_key in ['Perceived', 'Max', 'Min', 'Temperature']:
+                    new_value = f'{value} °C'
+                elif new_key == 'Wind - degrees':
+                    new_key = 'Wind - Degrees'
+                    new_value = f'{value}°'
+                elif new_key == 'Wind - speed':
+                    new_key = 'Wind - Speed'
+                    new_value = f'{value} km/h'
+                elif new_key == 'Sunrise' or new_key == 'Sunset':
+                    # Convert the timestamp to a datetime object
+                    dt_object = datetime.fromtimestamp(value)
 
-                # Format the datetime object as a string
-                new_value = dt_object.strftime("%H:%M:%S")
-            elif "rowth" in new_key or "rate" in new_key:
-                new_value = f'{value} %'
+                    # Format the datetime object as a string
+                    new_value = dt_object.strftime("%H:%M:%S")
+                elif "rowth" in new_key or "rate" in new_key:
+                    new_value = f'{value} %'
+                else:
+                    new_value = value
+                formatted_weather_json[new_key] = new_value
             else:
-                new_value = value
-            formatted_weather_json[new_key] = new_value
-        else:
-            ic(f"{wkey} not in key_mapping")
-    
-    context["weather"] = formatted_weather_json
+                ic(f"{wkey} not in key_mapping")
+        context["weather"] = formatted_weather_json
 
     # World Time
-    world_time_ninjas_api_response = requests.get(f"https://api.api-ninjas.com/v1/worldtime?city={country_data['Capital']}&country={country_code}", headers=headers)
-    world_time_json = world_time_ninjas_api_response.json()
     if world_time_json:
         ic(world_time_json)
         context["country_time"] = {"Local_Time": world_time_json.get("datetime"), "Time_Zone": world_time_json.get("timezone")}
-
 
     return render(request, 'country.html', context)
