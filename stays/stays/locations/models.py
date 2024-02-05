f1f1c259db0a487b2e99ed11a001eee18ce9b069
nojoven <work.cedricjoseph@gmail.com>
2024-02-06 00:55:58 +0100

    comment unused models in locations

--- stays/stays/locations/models.py
+++ stays/stays/locations/models.py
@@ -49,108 +49,88 @@ class StayCountry(models.Model):
     #     super().save(*args, **kwargs)
 
 
-class StayCountryHasUpvotes(models.Model):
-    country_of_stay = models.ForeignKey(Country, null=True, on_delete=models.CASCADE)
-    upvotes_count = models.IntegerField(
-        default=0,
-        validators=[MinValueValidator(0, message="Upvotes count cannot be negative.")]
-    )
+# class StayCountryHasUpvotes(models.Model):
+#     country_of_stay = models.ForeignKey(Country, null=True, on_delete=models.CASCADE)
+#     upvotes_count = models.IntegerField(
+#         default=0,
+#         validators=[MinValueValidator(0, message="Upvotes count cannot be negative.")]
+#     )
 
 
-# Signal pour mettre à jour StayCountryHasUpvotes après chaque sauvegarde de StayCountry
-# @receiver(post_save, sender=StayCountry)
-# def update_upvotes_count(sender, instance, **kwargs):
-#     stay_country_upvotes, created = StayCountryHasUpvotes.objects.get_or_create(country_of_stay=instance.country_name)
-#     if not created:
-#         stay_country_upvotes.upvotes_count += 1
-#         stay_country_upvotes.save()
+# class UpvoteCountryRanking(models.Model):
+#     country_name = models.CharField(max_length=255, null=True)
+#     total_upvotes = models.IntegerField(
+#         default=0,
+#         validators=[MinValueValidator(0, message="Total upvotes cannot be negative.")]
+#     )
 
 
-
-class UpvoteCountryRanking(models.Model):
-    country_name = models.CharField(max_length=255, null=True)
-    total_upvotes = models.IntegerField(
-        default=0,
-        validators=[MinValueValidator(0, message="Total upvotes cannot be negative.")]
-    )
-
-# @receiver(post_save, sender=StayCountryHasUpvotes)
-# def update_top_upvoted_country(sender, instance, **kwargs):
-#     country_name = instance.country_of_stay  # Assurez-vous que le nom du pays est correctement récupéré
-#     country_ranking_entry, created = UpvoteCountryRanking.objects.get_or_create(country_name=country_name)
-
-#     if not created:
-#         # Mettez à jour la somme des upvotes_count pour ce pays
-#         country_ranking_entry.total_upvotes = StayCountryHasUpvotes.objects.filter(country_of_stay__name=country_name).aggregate(models.Sum('upvotes_count'))['upvotes_count__sum']
-#         country_ranking_entry.save()
+# class StayChallenge(models.Model):
+#     uuid = models.UUIDField(default=uuid_generator, editable=False)
+#     publication_identifier = models.ForeignKey(Publication, on_delete=models.CASCADE)
+#     challenge_text = models.CharField(max_length=255, default="Guess where it happened :", null=True)
+#     is_open = models.BooleanField(default=False)
+#     right_answer = models.ForeignKey(City, on_delete=models.CASCADE, null=True)
+#     max_number_of_attempts = models.IntegerField(default=5)
+#     has_winner = models.BooleanField(default=False)
+#     winner_slug = models.SlugField(max_length=500, null=True)
+#     start_date = models.DateTimeField(null=True)
+#     date_of_success = models.DateTimeField(null=True)
+#     end_date_limit = models.DateTimeField(null=True)
 
 
-class StayChallenge(models.Model):
-    uuid = models.UUIDField(default=uuid_generator, editable=False)
-    publication_identifier = models.ForeignKey(Publication, on_delete=models.CASCADE)
-    challenge_text = models.CharField(max_length=255, default="Guess where it happened :", null=True)
-    is_open = models.BooleanField(default=False)
-    right_answer = models.ForeignKey(City, on_delete=models.CASCADE, null=True)
-    max_number_of_attempts = models.IntegerField(default=5)
-    has_winner = models.BooleanField(default=False)
-    winner_slug = models.SlugField(max_length=500, null=True)
-    start_date = models.DateTimeField(null=True)
-    date_of_success = models.DateTimeField(null=True)
-    end_date_limit = models.DateTimeField(null=True)
+# class ChallengeAttempt(models.Model):
+#     uuid = models.UUIDField(default=uuid_generator, editable=False)
+#     challenge = models.ForeignKey(StayChallenge, on_delete=models.CASCADE, null=True)
+#     profile_of_attempt = SlugFieldForeignKey(Profile, max_length=500, null=True, on_delete=models.CASCADE, to_field="slug")
+#     date_of_attempt = models.DateTimeField(auto_now_add=True)
+#     incrementation_number = models.IntegerField(default=1)
+#     number_of_attempts = models.IntegerField(
+#         default=0,
+#         validators=[
+#             MinValueValidator(0, message="Number of attempts cannot be negative."),
+#             MaxValueValidator(1, message="Number of attempts cannot exceed the maximum allowed."),
+#         ]
+#     )
+#     answer_of_profile = models.ForeignKey(City, on_delete=models.CASCADE, null=True)
+#     is_answer_correct = models.BooleanField(default=False)
+
+#     def save(self, *args, **kwargs):
+#         # Vérifier si la réponse est correcte lors de chaque sauvegarde
+#         if self.answer_of_profile == self.challenge.right_answer:
+#             # Mettre à jour les valeurs de StayChallenge
+#             self.is_answer_correct = True
+#             self.challenge.has_winner = True
+#             self.challenge.is_open = False
+#             self.challenge.winner_slug = self.profile_of_attempt
+#             self.challenge.date_of_success = self.date_of_attempt
+#             self.challenge.end_date_limit = self.date_of_attempt
+
+#             # Update ProfileRemainingAttempts
+#             remaining_attempts_instance, _ = ProfileRemainingAttempts.objects.get_or_create(
+#                 challenge_id=self.challenge_id,
+#                 profile_slug=self.profile_of_attempt.slug,
+#                 defaults={'remaining_attempts': self.challenge.max_number_of_attempts}
+#             )
+
+#             ProfileRemainingAttempts.objects.filter(
+#                 challenge_id=self.challenge_id,
+#                 profile_slug=self.profile_of_attempt.slug
+#             ).update(
+#                 remaining_attempts=models.F('remaining_attempts') - 1
+#             )
+
+#         super().save(*args, **kwargs)
 
 
-class ChallengeAttempt(models.Model):
-    uuid = models.UUIDField(default=uuid_generator, editable=False)
-    challenge = models.ForeignKey(StayChallenge, on_delete=models.CASCADE, null=True)
-    profile_of_attempt = SlugFieldForeignKey(Profile, max_length=500, null=True, on_delete=models.CASCADE, to_field="slug")
-    date_of_attempt = models.DateTimeField(auto_now_add=True)
-    incrementation_number = models.IntegerField(default=1)
-    number_of_attempts = models.IntegerField(
-        default=0,
-        validators=[
-            MinValueValidator(0, message="Number of attempts cannot be negative."),
-            MaxValueValidator(1, message="Number of attempts cannot exceed the maximum allowed."),
-        ]
-    )
-    answer_of_profile = models.ForeignKey(City, on_delete=models.CASCADE, null=True)
-    is_answer_correct = models.BooleanField(default=False)
+# class AttemptHasLocationChallenge(models.Model):
+#     attempt = models.ForeignKey(ChallengeAttempt, on_delete=models.CASCADE, to_field='id', null=True)
+#     challenge = models.ForeignKey(StayChallenge, on_delete=models.CASCADE, to_field='id', null=True)
 
-    def save(self, *args, **kwargs):
-        # Vérifier si la réponse est correcte lors de chaque sauvegarde
-        if self.answer_of_profile == self.challenge.right_answer:
-            # Mettre à jour les valeurs de StayChallenge
-            self.is_answer_correct = True
-            self.challenge.has_winner = True
-            self.challenge.is_open = False
-            self.challenge.winner_slug = self.profile_of_attempt
-            self.challenge.date_of_success = self.date_of_attempt
-            self.challenge.end_date_limit = self.date_of_attempt
+#     class Meta:
+#         unique_together = ('attempt', 'challenge')
 
-            # Update ProfileRemainingAttempts
-            remaining_attempts_instance, _ = ProfileRemainingAttempts.objects.get_or_create(
-                challenge_id=self.challenge_id,
-                profile_slug=self.profile_of_attempt.slug,
-                defaults={'remaining_attempts': self.challenge.max_number_of_attempts}
-            )
-
-            ProfileRemainingAttempts.objects.filter(
-                challenge_id=self.challenge_id,
-                profile_slug=self.profile_of_attempt.slug
-            ).update(
-                remaining_attempts=models.F('remaining_attempts') - 1
-            )
-
-        super().save(*args, **kwargs)
-
-
-class AttemptHasLocationChallenge(models.Model):
-    attempt = models.ForeignKey(ChallengeAttempt, on_delete=models.CASCADE, to_field='id', null=True)
-    challenge = models.ForeignKey(StayChallenge, on_delete=models.CASCADE, to_field='id', null=True)
-
-    class Meta:
-        unique_together = ('attempt', 'challenge')
-
-class ProfileRemainingAttempts(models.Model):
-    challenge_id = NullableBigIntegerFieldForeignKey(StayChallenge, on_delete=models.CASCADE, to_field='id', null=True)
-    profile_slug = SlugFieldForeignKey(Profile, on_delete=models.CASCADE, to_field='slug', null=True)
-    remaining_attempts = models.IntegerField(default=5)
+# class ProfileRemainingAttempts(models.Model):
+#     challenge_id = NullableBigIntegerFieldForeignKey(StayChallenge, on_delete=models.CASCADE, to_field='id', null=True)
+#     profile_slug = SlugFieldForeignKey(Profile, on_delete=models.CASCADE, to_field='slug', null=True)
+#     remaining_attempts = models.IntegerField(default=5)
