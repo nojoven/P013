28e0024ce75144af9fde674781d956279a439ec5
nojoven <work.cedricjoseph@gmail.com>
2024-02-05 23:02:19 +0100

    tests: second ut

--- stays/stays/stays/tests/test_unitaire_stays.py
+++ stays/stays/stays/tests/test_unitaire_stays.py
@@ -1,7 +1,7 @@
 import pytest
 from django.http import HttpRequest
 from users.utils import uuid_generator
-from stays.api import is_profile_online
+from stays.api import is_profile_online, get_names_from_country_code
 from django.conf import settings
 # Configure Django settings
 settings.configure()
@@ -27,3 +27,20 @@ def test_is_profile_online(mocker):
 
     # Check that the mock was called with the correct slug
     mock_profile.assert_called_once_with(slug=slug)
+
+
+def test_get_names_from_country_code(mocker):
+    # Setup
+    country_code = 'US'
+    request = HttpRequest()
+    mock_stay_country = mocker.patch('locations.models.StayCountry.objects.get')
+    mock_stay_country.return_value = mocker.MagicMock(country_name='United States', continent_name='North America')
+
+    # Call the function
+    response = get_names_from_country_code(request, country_code)
+
+    # Check the result
+    assert response == {"country": 'United States', "continent": 'North America'}
+
+    # Check that the mock was called with the correct country code
+    mock_stay_country.assert_called_once_with(country_code_of_stay=country_code)
