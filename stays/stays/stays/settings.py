c0d789ce1043aec619c936bca679fac9e3eb7e3e
nojoven <work.cedricjoseph@gmail.com>
2024-02-07 15:20:02 +0100

    comment logging

--- stays/stays/stays/settings.py
+++ stays/stays/stays/settings.py
@@ -72,25 +72,25 @@ if DEBUG is True:
 ALLOWED_HOSTS = ["*", "localhost", "0.0.0.0", "127.0.0.1"]
 
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'root': {
-        'handlers': ['console'],
-        'level': 'DEBUG',
-    },
-    'loggers': {
-        'django.db.backends': {
-            # 'level': 'DEBUG',
-            'level': 'INFO',
-        },
-    },
-}
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'handlers': {
+#         'console': {
+#             'class': 'logging.StreamHandler',
+#         },
+#     },
+#     'root': {
+#         'handlers': ['console'],
+#         'level': 'DEBUG',
+#     },
+#     'loggers': {
+#         'django.db.backends': {
+#             # 'level': 'DEBUG',
+#             'level': 'INFO',
+#         },
+#     },
+# }
 
 
 # Application definition
