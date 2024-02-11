2329792aa6c7a764a1edc372189aef4586e72a49
nojoven <work.cedricjoseph@gmail.com>
2024-02-11 15:21:21 +0100

    comment unused

--- stays/stays/users/forms.py
+++ stays/stays/users/forms.py
@@ -4,7 +4,7 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm, Password
 from django_countries.widgets import CountrySelectWidget
 from django_countries.fields import CountryField
 from django.contrib.auth.forms import PasswordResetForm as DjangoPasswordResetForm
-from django.core.mail import send_mail
+# from django.core.mail import send_mail
 from django.contrib.auth.tokens import default_token_generator
 from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
