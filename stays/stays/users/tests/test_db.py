43436b87d1029bd3c300b8626d12387b0b971255
nojoven <work.cedricjoseph@gmail.com>
2024-02-16 13:21:23 +0100

    tests: profile

--- stays/stays/users/tests/test_db.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from ..models import Profile
-
-
-# Create your tests here.
-
-def test_can_select_all_users():
-    profiles = Profile.objects.all().filter(is_superuser=False)
-    assert profiles is not None
