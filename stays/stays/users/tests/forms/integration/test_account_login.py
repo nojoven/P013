6f823bb3e73687ae1750b1e0947b06cf17b11e5b
nojoven <work.cedricjoseph@gmail.com>
2024-02-16 19:40:58 +0100

    tests: integration

---
+++ stays/stays/users/tests/forms/integration/test_account_login.py
@@ -0,0 +1,61 @@
+from django_webtest import WebTest
+from django.urls import reverse
+from model_bakery import baker
+from django.contrib.auth import get_user_model
+from icecream import ic
+import re
+
+class TestAccountLoginForm(WebTest):
+    def setUp(self):
+        # Créez des instances de modèles nécessaires pour les tests ici
+        self.user = baker.make('users.Profile', email='testuser@example.com')
+        self.user.set_password('password')  # Définissez le mot de passe en clair
+        self.user.save()
+
+    def test_form_valid(self):
+        # Préparez les données du formulaire
+        form_data = {
+            'email': 'testuser@example.com',
+            'password': 'password',
+        }
+
+        # Simulez une requête POST sur la vue de connexion
+        response = self.app.post(reverse('users:login'), params=form_data)
+
+        # Vérifiez que la réponse est correcte
+        self.assertEqual(response.status_code, 200)
+
+    def test_form_no_email(self):
+        # Préparez les données du formulaire
+        form_data = {
+            'password': 'password',
+        }
+
+        # Simulez une requête POST sur la vue de connexion
+        response = self.app.post(reverse('users:login'), params=form_data)
+        # Utilisez une expression régulière pour extraire le titre de la page
+        match = re.search('<title>(.*?)</title>', response.text)
+
+        # Vérifiez que le titre de la page est toujours "Sign In"
+        if match:
+            title = match.group(1)
+            self.assertEqual(title, 'sign in')
+
+    def test_form_no_password(self):
+        # Préparez les données du formulaire
+        form_data = {
+            'email': 'testuser@example.com',
+        }
+
+        # Simulez une requête POST sur la vue de connexion
+        response = self.app.post(reverse('users:login'), params=form_data)
+        
+        # Simulez une requête POST sur la vue de connexion
+        response = self.app.post(reverse('users:login'), params=form_data)
+        # Utilisez une expression régulière pour extraire le titre de la page
+        match = re.search('<title>(.*?)</title>', response.text)
+
+        # Vérifiez que le titre de la page est toujours "Sign In"
+        if match:
+            title = match.group(1)
+            self.assertEqual(title, 'sign in')
