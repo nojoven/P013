0f030367de80fff8143aaad59aedb8af743c8753
nojoven <work.cedricjoseph@gmail.com>
2024-02-18 18:35:57 +0100

    tests: publish form

--- stays/stays/users/tests/forms/integration/test_publish_content.py
+++ stays/stays/users/tests/forms/integration/test_publish_content.py
@@ -1,21 +1,36 @@
 import pytest
 from django.core.files.uploadedfile import SimpleUploadedFile
+from django.contrib.auth.hashers import make_password
+from model_bakery import baker
 from faker import Faker
 from users.forms import PublishContentForm
+from core.models import Publication
+from users.models import Profile
+
 
 fake = Faker()
 
-# @pytest.fixture
-# def publication():
-#     return baker.make(Publication)
 
+@pytest.mark.django_db
 @pytest.fixture
-def form_data():
+def profile():
+    return Profile.objects.create_user(username='testuser', email='test@example.com', password=make_password('testpassword'))
+
+
+@pytest.mark.django_db
+@pytest.fixture
+def publication(profile):
+    return baker.make(Publication, author_slug=profile.slug, author_username=profile.username)
+
+
+@pytest.mark.django_db
+@pytest.fixture
+def form_data(publication, profile):
     return {
         'content_type': 'text',
         'title': fake.sentence(),
-        'author_slug': fake.slug(),
-        'author_username': fake.user_name(),
+        'author_slug': profile.slug,
+        'author_username': profile.username,
         'country_code_of_stay': 'US',
         'published_from_country_code': 'IT',
         'summary': fake.text(),
@@ -26,66 +41,79 @@ def form_data():
         'voice_story': SimpleUploadedFile('voice_story.mp3', b'file_content', content_type='audio/mpeg'),
     }
 
+@pytest.mark.django_db
 def test_form_invalid_with_both_text_and_voice(form_data):
     form = PublishContentForm(data=form_data)
     assert not form.is_valid()
 
+@pytest.mark.django_db
 def test_form_invalid_without_content_type(form_data):
     form_data.pop('content_type')
     form = PublishContentForm(data=form_data)
     assert not form.is_valid()
 
+@pytest.mark.django_db
 def test_form_invalid_without_title(form_data):
     form_data.pop('title')
     form = PublishContentForm(data=form_data)
     assert not form.is_valid()
 
+@pytest.mark.django_db
 def test_form_invalid_without_author_slug(form_data):
     form_data.pop('author_slug')
     form = PublishContentForm(data=form_data)
     assert not form.is_valid()
 
+@pytest.mark.django_db
 def test_form_invalid_without_author_username(form_data):
     form_data.pop('author_username')
     form = PublishContentForm(data=form_data)
     assert not form.is_valid()
 
+@pytest.mark.django_db
 def test_form_invalid_without_country_code_of_stay(form_data):
     form_data.pop('country_code_of_stay')
     form = PublishContentForm(data=form_data)
     assert not form.is_valid()
 
+@pytest.mark.django_db
 def test_form_invalid_without_published_from_country_code(form_data):
     form_data.pop('published_from_country_code')
     form = PublishContentForm(data=form_data)
     assert not form.is_valid()
 
+@pytest.mark.django_db
 def test_form_invalid_without_summary(form_data):
     form_data.pop('summary')
     form = PublishContentForm(data=form_data)
     assert not form.is_valid()
 
+@pytest.mark.django_db
 def test_form_invalid_without_picture(form_data):
     form_data.pop('picture')
     form = PublishContentForm(data=form_data)
     assert not form.is_valid()
 
+@pytest.mark.django_db
 def test_form_invalid_without_year_of_stay(form_data):
     form_data.pop('year_of_stay')
     form = PublishContentForm(data=form_data)
     assert not form.is_valid()
 
+@pytest.mark.django_db
 def test_form_invalid_without_story(form_data):
     form_data['text_story'] = None
     form_data['voice_story'] = None
     form = PublishContentForm(data=form_data)
     assert not form.is_valid()
 
+@pytest.mark.django_db
 def test_form_valid_with_text_story(form_data):
     form_data['voice_story'] = None
     form = PublishContentForm(data=form_data)
     assert not form.is_valid()
 
+@pytest.mark.django_db
 def test_form_valid_with_voice_story(form_data):
     form_data['text_story'] = None
     form = PublishContentForm(data=form_data)
