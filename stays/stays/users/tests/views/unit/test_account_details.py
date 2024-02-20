6f43cf79f539b6190c1a5684182986613e27872d
nojoven <work.cedricjoseph@gmail.com>
2024-02-20 01:23:02 +0100

    tests: account details

--- stays/stays/users/tests/views/unit/test_account_details.py
+++ stays/stays/users/tests/views/unit/test_account_details.py
@@ -6,7 +6,8 @@ from unittest.mock import patch
 from stays.utils.common_helpers import uuid_generator
 from django.contrib.auth import get_user_model
 from icecream import ic
-
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404
 User = get_user_model()
 
 @pytest.mark.django_db
@@ -20,24 +21,19 @@ class TestAccountDetailsView(TestCase):
         self.user.save()
         self.url = reverse('users:account', kwargs={'slug': self.user.slug})
 
-    
+
     @patch('django.contrib.auth.mixins.LoginRequiredMixin', new_callable=lambda: object)
     @patch('users.signals.update_user_status')
     def test_account_details_with_authenticated_user(self, mock_update_user_status, mock_mixin):
-        # ic(self.user.is_authenticated)
-        # ic(User.objects.get(email=self.user.email).slug)
-        # ic(User.objects.get(email=self.user.email).username)
-        # ic(User.objects.get(email=self.user.email).password)
+
         # self.client.login(email=self.user.email, password='testpasswordP9671!')  # Log in the user
         self.client.force_login(self.user)
-        # ic(self.url)
-        # self.user.is_athenticated = True
+
         response = self.client.get(self.url, secure=False)
-        # ic(response.status_code)
-        # ic(response)
+
         self.assertEqual(response.status_code, 200)  # Check that the user can access the page
         self.assertTemplateUsed(response, 'account.html')  # Check the template
-        # ic(response.location)
+
         self.assertIn('current_user', response.context)  # Check the context
         self.assertEqual(response.context['current_user'], self.user)  # Check the current user
         self.assertIsInstance(response.context['current_user'], User)  # Check the current user
