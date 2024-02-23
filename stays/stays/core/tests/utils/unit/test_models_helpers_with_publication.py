fd3f6251c358890a847554b03f4538dcd333850c
nojoven <work.cedricjoseph@gmail.com>
2024-02-23 21:00:23 +0100

    done

--- stays/stays/core/tests/utils/unit/test_models_helpers_with_publication.py
+++ stays/stays/core/tests/utils/unit/test_models_helpers_with_publication.py
@@ -1,4 +1,3 @@
-# Importations nécessaires pour les tests
 import pytest
 from datetime import datetime
 from model_bakery import baker
@@ -6,14 +5,13 @@ from core.models import Publication
 from users.models import Profile
 from core.utils.models_helpers import voice_story_upload_to, picture_upload_to
 from stays.utils.common_helpers import uuid_generator
-from icecream import ic
+from django.urls import reverse
 
 @pytest.mark.django_db
-# Test de la fonction voice_story_upload_to
-def test_voice_story_upload_to():
+def test_publication_instance_model_helpers():
     # Création d'un profil
-    profile = baker.make(Profile, 
-    email='testadmin@example.com', 
+    profile = baker.make(Profile,
+    email='testadmin@example.com',
     slug = f"'testadmin@example.com'{uuid_generator()}",
     username = 'testadmin',
     profile_picture = 'picture.jpg'
@@ -27,24 +25,11 @@ def test_voice_story_upload_to():
     # Vérification que voice_story_upload_to renvoie le bon chemin
     assert voice_story_upload_to(publication, 'voice.mp3') == f'uploads/{publication.author_slug}/{datetime.now().strftime("%Y/%m/%d")}/{publication.uuid}/voice/voice.mp3'
 
-@pytest.mark.django_db
-# Test de la fonction picture_upload_to
-def test_picture_upload_to():
-    # Création d'un profil
-    profile = baker.make(Profile,
-    email='testadmin@example.com',
-    slug = f"'testadmin@example.com'{uuid_generator()}",
-    username = 'testadmin',
-    profile_picture = 'picture.jpg'
-    )
-    profile.set_password('testpasYEF93*&5sword')
-    profile.save()
-
-    # Création d'une instance de Publication
-    publication = baker.make(Publication, title='Title One', author_username=profile.username, author_slug=profile.slug, picture='picture.jpg')
-    publication.save()
-    ic(publication)
-    ic(Publication.objects.all())
-
     # Vérification que picture_upload_to renvoie le bon chemin
     assert picture_upload_to(publication, 'picture.jpg') == f'uploads/{publication.author_slug}/{datetime.now().strftime("%Y/%m/%d")}/{publication.uuid}/picture/picture.jpg'
+
+    # Vérification de la méthode __str__
+    assert str(publication) == 'Title One'
+
+    # Vérification de la méthode get_absolute_url
+    assert publication.get_absolute_url() == reverse('users:account', args=[publication.author_slug])
