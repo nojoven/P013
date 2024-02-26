cda2a81980fbe842f070e19ee54c067e4fbd27cc
nojoven <work.cedricjoseph@gmail.com>
2024-02-26 15:55:16 +0100

    fix: performances improve loading audio

--- stays/stays/core/urls.py
+++ stays/stays/core/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     # /home
     path("", views.home, name="home"),
     path('publications/publication/<uuid:publication_uuid>/picture', views.serve_publication_picture, name='serve_publication_picture'),
+    path('publications/publication/<uuid:publication_uuid>/audio', views.serve_publication_audio, name='serve_publication_audio'),
     path("publications/publication/rm", views.PublicationDeleteView.as_view(), name="delete_publication"),
     path("publications/publication/<uuid:uuid>", views.PublicationDetailView.as_view(), name="publication"),
     path("publications/publication/<uuid:pk>/edit", views.PublicationUpdateView.as_view(), name="edit_publication"),
