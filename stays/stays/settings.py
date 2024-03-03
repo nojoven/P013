823c543290ed7cfb017d87548e0ab6f10cc11382
nojoven <work.cedricjoseph@gmail.com>
2024-03-03 15:09:04 +0100

    remove silk

--- stays/stays/settings.py
+++ stays/stays/settings.py
@@ -138,7 +138,7 @@ CITIES_LIGHT_TRANSLATION_LANGUAGES = ['fr', 'en']
 
 
 MIDDLEWARE = [
-    'silk.middleware.SilkyMiddleware',
+    # 'silk.middleware.SilkyMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -225,7 +225,7 @@ CACHES = {
     # … default cache config and others
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": "redis://:28xsyaxJM88ON55q1L2HM38hrCeC4vo5@redis-18488.c1.asia-northeast1-1.gce.cloud.redislabs.com:18488/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
