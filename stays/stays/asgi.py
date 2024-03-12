c724bce5b3a5dc76950c421fe954c2f8da41ce08
nojoven <work.cedricjoseph@gmail.com>
2024-03-12 18:14:01 +0100

    fix: update asgi

--- stays/stays/asgi.py
+++ stays/stays/asgi.py
@@ -11,10 +11,10 @@ import os
 
 from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
-
+from stays.stays import settings
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stays.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings.__name__)
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter(
