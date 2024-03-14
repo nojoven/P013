80a4d167f8a340c36817c8024b9c7be72d5aa4af
nojoven <work.cedricjoseph@gmail.com>
2024-03-14 22:13:27 +0100

    feat: get media files from cloud storage

--- stays/stays/settings.py
+++ stays/stays/settings.py
@@ -70,7 +70,6 @@ LOGGING = {
 # Application definition
 
 INSTALLED_APPS = [
-    # APM
     "django.contrib.sites",
     "django.contrib.admin",
     "daphne",
@@ -98,6 +97,7 @@ INSTALLED_APPS = [
     "friendship",
     "pagination",
     "corsheaders",
+    "storages",
 ]
 
 
@@ -296,25 +296,46 @@ USE_I18N = True
 
 USE_TZ = True
 
+# ...
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_URL = "/static/"
 COUNTRIES_FLAG_URL = "flags/{code}.png"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 GEOIP_PATH = os.path.join(BASE_DIR, "geoip")
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_AUTO_FIELD = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-
 AUTH_USER_MODEL = "users.Profile"
 
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
+
+if DEBUG is False:
+    # settings
+    AWS_ACCESS_KEY_ID = getenv('SPACE_ACCESS_KEY')
+    AWS_SECRET_ACCESS_KEY = getenv('SPACE_SECRET_KEY')
+    AWS_STORAGE_BUCKET_NAME = getenv('SPACE_NAME')
+    AWS_DEFAULT_ACL = 'public-read'
+    AWS_S3_ENDPOINT_URL = 'https://stays.fra1.digitaloceanspaces.com'
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    # static settings
+    AWS_LOCATION = 'static'
+    MEDIA_LOCATION = 'media'
+    STATIC_URL = f'https://{AWS_S3_ENDPOINT_URL}/{AWS_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_ENDPOINT_URL}/{MEDIA_LOCATION}/'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    STATIC_ROOT = Path(BASE_DIR) / 'staticfiles'
+else:
+    STATIC_URL = '/static/'
+    STATIC_ROOT = Path(BASE_DIR) / 'staticfiles'
+
+STATICFILES_DIRS = (Path(BASE_DIR) / 'static',)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = Path(BASE_DIR) / 'mediafiles'
 
 AUTHENTICATION_BACKENDS = [
     "users.backends.EmailBackend",  # custom backend
